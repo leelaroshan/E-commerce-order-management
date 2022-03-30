98933f89c9f67759ceceac124168e503690c5e55
Leelavathi Gade <leelaroshangade@gmail.com>
2022-03-30 22:42:54 +0530

    boiler plate

---
+++ assignments/e-commerce-order-management/src/App.js
@@ -0,0 +1,17 @@
+
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <ul>
+          <li>Products</li>
+          <li>Orders</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
